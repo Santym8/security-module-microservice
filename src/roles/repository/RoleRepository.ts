fc1c63a121e4d0518c7ede296ada184d98c770c6
BLeo16 <bleonardo1621@gmail.com>
2023-12-25 14:27:02 -0500

    API Roles administration developed

---
+++ src/roles/repository/RoleRepository.ts
@@ -0,0 +1,32 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { DataSource, Repository } from "typeorm";
+import { Role } from "../model/Role.entity";
+
+@Injectable()
+export class RoleRepository {
+
+    private roleRepository: Repository<Role>;
+
+    constructor(
+        @Inject('DATA_SOURCE')
+        private readonly dataSourceConfig: DataSource,
+    ) {
+        this.roleRepository = this.dataSourceConfig.getRepository(Role);
+    }
+
+    public async createOrUpdate(role: Role): Promise<Role> {
+        return this.roleRepository.save(role);
+    }
+    public async delete(id: number): Promise<void> {
+        await this.roleRepository.delete({ id: id });
+    }
+    public async getAll(): Promise<Role[]> {
+        return await this.roleRepository.find();
+    }
+    public async getById(id: number): Promise<Role> {
+        return await this.roleRepository.findOneBy({ id: id });
+    }
+    public async getByName(name: string): Promise<Role> {
+        return await this.roleRepository.findOneBy({ name: name });
+    }
+}
