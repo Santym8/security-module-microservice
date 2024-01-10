f9d6e9c620f0e544a74779c1ec26a1160cfd785b
BLeo16 <bleonardo1621@gmail.com>
2024-01-09 21:52:45 -0500

    Add new security functions to PopulateFunctionData

--- src/config/populate-data/PopulateFunctionData.ts
+++ src/config/populate-data/PopulateFunctionData.ts
@@ -19,6 +19,108 @@ export class PopulateFunctionData {
                 name: 'SEC-LOGIN',
                 moduleName: "SECURITY"
             },
+            {
+                name: 'SEC-USERS-READ',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-USERS-CREATE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-USERS-UPDATE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-USERS-DELETE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-ROLES-READ',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-ROLES-CREATE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-ROLES-UPDATE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-ROLES-DELETE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-MODULES-READ',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-MODULES-CREATE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-MODULES-UPDATE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-MODULES-DELETE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-FUNCTIONS-READ',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-FUNCTIONS-CREATE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-FUNCTIONS-UPDATE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-FUNCTIONS-DELETE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-FUNCTIONS-TO-ROLE-READ',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-FUNCTIONS-TO-ROLE-UPDATE',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-ROLES-TO-USER-READ',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-ROLES-TO-USER-UPDATE',
+                moduleName: "SECURITY"
+            },
+            {
+                name:'SEC-AUDIT-READ',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-AUDIT-USER-REPORT',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-USERS-ROLES-FUNCTIONS-MODULES-REPORT',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-ROLES-FUNCTION-MODULES-REPORT',
+                moduleName: "SECURITY"
+            },
+            {
+                name: 'SEC-MODULES-FUNCTIONS-REPORT',
+                moduleName: "SECURITY"
+            }
+
+
         ]
 
         await Promise.all(functions.map(async (fun) => {
