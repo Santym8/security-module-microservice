313f05905f53fc181c6dd91211585e8d4f96eccf
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-01-10 16:50:15 -0500

    Remove audit functionality

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,14 +4,10 @@ import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './service/AuthService';
 import { AuthController } from './controller/AuthController';
 import { TokenManager } from './utils/TokenManager';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthGuard } from './utils/AuthGuard';
-import { AuditModule } from 'src/audit/audit.module';
 
 @Module({
     imports: [
         UsersModule,
-        forwardRef(() => AuditModule), // Avoid circular dependency
         JwtModule.register({
             global: true,
         }),
--- src/auth/service/AuthService.ts
+++ src/auth/service/AuthService.ts
@@ -3,7 +3,6 @@ import { UserRepository } from "src/users/repository/UserRepository";
 import { AuthException } from "../exception/AuthException";
 import { TokenManager } from "../utils/TokenManager";
 import { AuthReponse } from "../dto/response/AuthResponse";
-import { AuditRepository } from "src/audit/repository/AuditRepository";
 import { Audit } from "src/audit/model/Audit.entity";
 import { LoginRequest } from "../dto/request/LoginRequest";
 
@@ -11,8 +10,7 @@ import { LoginRequest } from "../dto/request/LoginRequest";
 export class AuthService {
     constructor(
         private readonly userRepository: UserRepository,
-        private readonly tokenManager: TokenManager,
-        private readonly auditRepository: AuditRepository
+        private readonly tokenManager: TokenManager
     ) { }
 
     async login(loginRequest: LoginRequest): Promise<AuthReponse> {
@@ -23,23 +21,12 @@ export class AuthService {
             throw new AuthException("User not found", 404);
         }
 
-        const audit: Audit = {
-            action: 'LOGIN',
-            description: '',
-            ip: loginRequest.ip,
-            date: new Date(),
-            user: user,
-        }
 
         if (user.password !== loginRequest.password) {
-            audit.description = 'Login failed, bad credentials';
-            await this.auditRepository.createOrUpdate(audit);
             throw new AuthException("Bad credentials", 401);
         }
 
         if (user.status === false) {
-            audit.description = 'Login failed, user is inactive';
-            await this.auditRepository.createOrUpdate(audit);
             throw new AuthException("User is inactive", 401);
         }
 
@@ -49,8 +36,6 @@ export class AuthService {
 
         const token = await this.tokenManager.newToken(payload);
 
-        audit.description = 'Login success';
-        await this.auditRepository.createOrUpdate(audit);
 
         const functions = await this.userRepository.getFunctionsOfUser(user.id);
         return {
