4a2b77d9fe65aa56cff7d576c860609ef08b33f0
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-01-10 17:30:12 -0500

    Add Global decorator to AuditModule

--- src/audit/audit.module.ts
+++ src/audit/audit.module.ts
@@ -1,10 +1,11 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Global, Module, forwardRef } from '@nestjs/common';
 import { AuditController } from './controller/AuditController';
 import { AuditRepository } from './repository/AuditRepository';
 import { AuditService } from './service/AuditService';
 import { UsersModule } from 'src/users/users.module';
 import { FunctionsModule } from 'src/functions/functions.module';
 
+@Global()
 @Module({
     imports: [
         UsersModule,
