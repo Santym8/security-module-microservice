f0bc0682c8a51ac0dc76654abfe1c92c5afa85dd
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-01-11 21:39:09 -0500

    Add getAll method to TestController

--- src/test/TestController..ts
+++ src/test/TestController..ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class TestController {
+    
     @Get()
     getAll(): string {
         return "Hello From SECURITY MODULE";
